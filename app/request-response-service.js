403179115ab18c8137af99a1b851de8d54ed6e31
Robert Hardy <rhardy@scramsystems.com>
2018-08-13 08:31:24 -0600

    Refactor requestResponseService to remove evnt bindings for data and end
    - If a request requires these event bindings in the future I can always
      put them back, but for now they are unecessary.

--- app/request-response-service.js
+++ app/request-response-service.js
@@ -7,24 +7,21 @@ let requestResponseService = {
         const parsedUrl = url.parse(request.url, parseQueryString);
         const path = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
 
-        request.on('data', () => {}); // why is this needed if I don't care about payload?
+        let handler = router.getHandler(path);
 
-        request.on('end', () => {
-            let handler = router.getHandler(path);
-            handler((statusCode, payload) => {
-                const defaultStatusCode = 200;
-                const defaultPayload = {};
-    
-                statusCode = typeof (statusCode) === 'number' ? statusCode : defaultStatusCode;
-                payload = typeof (payload) === 'object' ? payload : defaultPayload;
-                const payloadString = JSON.stringify(payload);
-    
-                response.setHeader('content-type', 'application/json');
-                response.writeHead(statusCode);
-                response.end(payloadString);
-    
-                console.log('Request received:', path, 'Response Sent:', statusCode, payloadString);
-            });
+        handler((statusCode, payload) => {
+            const defaultStatusCode = 200;
+            const defaultPayload = {};
+
+            statusCode = typeof (statusCode) === 'number' ? statusCode : defaultStatusCode;
+            payload = typeof (payload) === 'object' ? payload : defaultPayload;
+            const payloadString = JSON.stringify(payload);
+
+            response.setHeader('content-type', 'application/json');
+            response.writeHead(statusCode);
+            response.end(payloadString);
+
+            console.log('Request received:', path, 'Response Sent:', statusCode, payloadString);
         });
     }
 };
